6a206bc7e6352a6090b68efebef42d9d09e17ff1
Jordi <jordi@iceverg.com>
2021-03-12 19:06:16 +0100

    added frontend routes

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './components/views/page-not-found/page-not-found.component';
+import { MoviesComponent } from './components/views/movies/movies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{ path: '', pathMatch: 'full', component: }
+  { path: '', pathMatch: 'full', redirectTo: '/movies'},
+  { path: 'movies', pathMatch: 'full', component: MoviesComponent},
+  { path: '404', component: PageNotFoundComponent },
+  { path: '**',  redirectTo: '/404', pathMatch: 'full'  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

6e8337128acbcb3e74a14f9927122534e5ad64a0
Jordi <jordi@iceverg.com>
2021-03-15 20:36:54 +0100

    added pwd check to Auth/login

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,11 +1,15 @@
 const userModel = require('../models/user.model');
+const bcrypt = require('bcrypt');
 
 module.exports = {
   login: (async (req, res, next) => {
-    console.log(req.body)
     let user = await userModel.findOne({email: req.body.email});
+    console.log(req.body);
     if (user) {
-      res.json();
+      if (bcrypt.compareSync(req.body.password, user.password)){
+        res.json();
+      }
+      res.status(400).json({error: 'Password invalid'});
     } else {
       res.status(404).json({error: 'User not found'});
     }

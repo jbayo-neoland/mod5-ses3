6471ae9f7dc45d039292d3af13d6c82306c5f2eb
Adonay Rodriguez <adonaydaniel16@gmail.com>
2021-03-24 21:24:40 +0100

    Back Adonay

--- server/scripts/verify-password.js
+++ server/scripts/verify-password.js
@@ -1,9 +1,9 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 let plainPassword = process.argv.slice(2)[0];
 let hashedPassword = process.argv.slice(2)[1];
 
-console.log('hashedPassword', process.argv);
+console.log("hashedPassword", process.argv);
 let isTheSame = bcrypt.compareSync(plainPassword, hashedPassword);
 
 console.log(isTheSame);
